6cbbf609167c75b72eed27beea6a9797bd28b335
Børre Gaup <albbas@gmail.com>
2018-12-03 20:24:16 +0100

    Make language and part of speech codes translatable

--- src/components/Stem.js
+++ src/components/Stem.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import { css } from 'react-emotion';
 import { Set } from 'immutable';
+import { t, Trans } from '@lingui/macro';
+
+const languages = [
+  t`eng`,
+  t`fin`,
+  t`lat`,
+  t`nob`,
+  t`nno`,
+  t`sme`,
+  t`sma`,
+  t`smj`,
+  t`smn`,
+  t`sms`,
+  t`swe`
+];
+
+const partOfSpeech = [
+  t`N`,
+  t`A`,
+  t`Adv`,
+  t`V`,
+  t`Pron`,
+  t`CS`,
+  t`CC`,
+  t`Adp`,
+  t`Po`,
+  t`Pr`,
+  t`Interj`,
+  t`Pcle`,
+  t`Num`,
+  t`ABBR`,
+  t`MWE`,
+];
 
 const korpLangs = Set.of('sma', 'sme', 'smj', 'smn', 'sms');
 
@@ -27,7 +60,7 @@ const Stem = ({ stem: {lemma, pos, lang, key}}) => {
       <span className={css({ display: 'inline'})}>{key ? <span className={css({
         fontWeight: 'bold',
         marginLeft: '2%'
-      })}>{lemma}</span> : lemma} {pos} {lang}</span>
+      })}>{lemma}</span> : lemma} <Trans id={pos} /> <Trans id={lang} /></span>
       {korpLangs.has(lang) && (KorpLink(lang, lemma))}
     </div>
   );
