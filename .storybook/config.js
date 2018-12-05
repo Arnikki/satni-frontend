a3db13c7195fd159cd28cc5979bd7db8fd658a87
Børre Gaup <albbas@gmail.com>
2018-12-05 12:34:48 +0100

    Manually load stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,18 @@
 import { configure } from '@storybook/react';
 
-function loadStories() {
-  require('../src/stories');
+function loadStories () {
+  require('../src/components/AdjParadigm.stories.js');
+  require('../src/components/DictArticle.stories.js');
+  require('../src/components/Examples.stories.js');
+  require('../src/components/Example.stories.js');
+  require('../src/components/NounParadigm.stories.js');
+  require('../src/components/PresentArticles.stories.js');
+  require('../src/components/SDTermArticle.stories.js');
+  require('../src/components/Source.stories.js');
+  require('../src/components/Stems.stories.js');
+  require('../src/components/Stem.stories.js');
+  require('../src/components/TermWikiArticle.stories.js');
+  require('../src/components/VerbParadigm.stories.js');
 }
 
 configure(loadStories, module);
