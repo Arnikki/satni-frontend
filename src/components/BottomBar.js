b518a6764781976bffe0a7371525d58799b1770a
Børre Gaup <albbas@gmail.com>
2018-12-10 12:43:22 +0100

    BottomBar with Typography

---
+++ src/components/BottomBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { Trans } from '@lingui/macro';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+  }
+};
+
+const BottomBar = ({ classes }) => (
+  <footer className={classes.footer}>
+    <Typography variant='body2' align='center'>
+      <Trans>
+        Sámi dictionaries and terms delivered by<br />
+        <a href='http://divvun.no'>
+        Divvun</a>, <a href='http://giella.org'>
+        Giellagáldu</a> and <a href='http://giellatekno.uit.no'>Giellatekno</a>
+      </Trans>
+    </Typography>
+  </footer>
+);
+
+BottomBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(BottomBar);
