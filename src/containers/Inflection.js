d3fdeaec03a1aece24218ebc8026a99d8c338c5a
Børre Gaup <albbas@gmail.com>
2018-12-13 17:20:52 +0100

    Handle inflection paths

--- src/containers/Inflection.js
+++ src/containers/Inflection.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import queryString from 'query-string';
-
+import Paradigm from '../containers/Paradigm';
 const Inflection = ({ match, location }) => {
   const parsed = queryString.parse(location.search);
 
-  return (
-    <div>
-      <div>Testing, 1, 2, 3</div>
-      <div>{match.params.lemma}</div>
-      <div>{parsed.lang}</div>
-      <div>{parsed.pos}</div>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <div>Testing, 1, 2, 3</div>
+  //     <div>{match.params.lemma}</div>
+  //     <div>{parsed.lang}</div>
+  //     <div>{parsed.pos}</div>
+  //   </div>
+  // );
+  return <Paradigm
+    lemma={match.params.lemma}
+    lang={parsed.lang}
+    pos={parsed.pos} />;
 };
 
 export default Inflection;
